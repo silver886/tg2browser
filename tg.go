e47a33a0e968be264252cf5d38b337f7ff2d5918
Leo Liu <karta1041337@gmail.com>
2019-10-20 18:22:46 +0800

    Investigate Telegram API

--- tg.go
+++ tg.go
@@ -28,14 +28,137 @@ func tgBot() {
 
 	for update := range updates {
 		if update.Message == nil { // ignore any non-Message Updates
+			log.Traceln("Non-message update:", update.Message)
 			continue
 		}
 
-		logTgBot.Debugf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		if update.Message.ReplyToMessage != nil {
+			log.Traceln("Reply to message:", update.Message.ReplyToMessage)
+			if update.Message.ReplyToMessage.From != nil {
+				log.Traceln("Reply to author:", update.Message.ReplyToMessage.From)
+			}
+		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
-
-		bot.Send(msg)
+		if update.Message.Text != "" {
+			logTgBot.Traceln("Text")
+			// tgBotText()
+			logTgBot.Debugln(update.Message.Text)
+		}
+		if update.Message.Audio != nil {
+			logTgBot.Traceln("Audio")
+			// tgBotAudio()
+			url, err := bot.GetFileDirectURL(update.Message.Audio.FileID)
+			logTgBot.Debugln(
+				update.Message.Audio,
+				url,
+				err,
+			)
+		}
+		if update.Message.Document != nil {
+			logTgBot.Traceln("Document")
+			// tgBotDocument()
+			url, err := bot.GetFileDirectURL(update.Message.Document.FileID)
+			logTgBot.Debugln(
+				update.Message.Document,
+				url,
+				err,
+			)
+		}
+		if update.Message.Animation != nil {
+			logTgBot.Traceln("Animation")
+			// tgBotAnimation()
+			url, err := bot.GetFileDirectURL(update.Message.Animation.FileID)
+			logTgBot.Debugln(
+				update.Message.Animation,
+				url,
+				err,
+			)
+		}
+		if update.Message.Game != nil {
+			logTgBot.Traceln("Game")
+			// tgBotGame()
+			logTgBot.Debugln(
+				update.Message.Game,
+			)
+		}
+		if update.Message.Photo != nil {
+			logTgBot.Traceln("Photo")
+			// tgBotPhoto()
+			logTgBot.Debugln(update.Message.Photo)
+			for i, p := range *update.Message.Photo {
+				url, err := bot.GetFileDirectURL(p.FileID)
+				logTgBot.Debugln(
+					i,
+					p,
+					url,
+					err,
+				)
+			}
+		}
+		if update.Message.Sticker != nil {
+			logTgBot.Traceln("Sticker")
+			// tgBotSticker()
+			url, err := bot.GetFileDirectURL(update.Message.Sticker.FileID)
+			logTgBot.Debugln(
+				update.Message.Sticker,
+				url,
+				err,
+			)
+		}
+		if update.Message.Video != nil {
+			logTgBot.Traceln("Video")
+			// tgBotVideo()
+			url, err := bot.GetFileDirectURL(update.Message.Video.FileID)
+			logTgBot.Debugln(
+				update.Message.Video,
+				url,
+				err,
+			)
+		}
+		if update.Message.Video != nil {
+			logTgBot.Traceln("Video")
+			// tgBotVideo()
+			url, err := bot.GetFileDirectURL(update.Message.Video.FileID)
+			logTgBot.Debugln(
+				update.Message.Video,
+				url,
+				err,
+			)
+		}
+		if update.Message.VideoNote != nil {
+			logTgBot.Traceln("VideoNote")
+			// tgBotVideoNote()
+			url, err := bot.GetFileDirectURL(update.Message.VideoNote.FileID)
+			logTgBot.Debugln(
+				update.Message.VideoNote,
+				url,
+				err,
+			)
+		}
+		if update.Message.Contact != nil {
+			logTgBot.Traceln("Contact")
+			// tgBotContact()
+			logTgBot.Debugln(update.Message.Contact)
+		}
+		if update.Message.Location != nil {
+			logTgBot.Traceln("Location")
+			// tgBotLocation()
+			logTgBot.Debugln(update.Message.Location)
+		}
+		if update.Message.Venue != nil {
+			logTgBot.Traceln("Venue")
+			// tgBotVenue()
+			logTgBot.Debugln(update.Message.Venue)
+		}
+		if update.Message.Invoice != nil {
+			logTgBot.Traceln("Invoice")
+			// tgBotInvoice()
+			logTgBot.Debugln(update.Message.Invoice)
+		}
+		if update.Message.SuccessfulPayment != nil {
+			logTgBot.Traceln("SuccessfulPayment")
+			// tgBotSuccessfulPayment()
+			logTgBot.Debugln(update.Message.SuccessfulPayment)
+		}
 	}
 }
---
+++ tg_text.go
@@ -0,0 +1,9 @@
+package main
+
+// if u, err := url.ParseRequestURI(update.Message.Text); err == nil {
+// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, u.String())
+// 	msg.ReplyToMessageID = update.Message.MessageID
+// 	bot.Send(msg)
+// } else {
+// 	logTgBot.WithError(err).Errorln("Not URL")
+// }
